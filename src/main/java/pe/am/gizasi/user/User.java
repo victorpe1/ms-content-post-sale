e980d9bfd7984c453a35d3ef6accd6742eca292f
victorpe1 <victor.pc.0009@gmail,com>
2024-06-11 22:32:05 -0500

    first commit

---
+++ src/main/java/pe/am/gizasi/user/User.java
@@ -0,0 +1,66 @@
+package pe.am.gizasi.user;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios", uniqueConstraints = {@UniqueConstraint(columnNames = {"userId"})})
+public class User implements UserDetails{
+	@Id
+	@GeneratedValue
+	 Integer id;
+	@Column(nullable = false)
+	 String username;
+	 String password;
+	 String firstname;
+	 String lastname;
+	 String country;
+	 @Enumerated(EnumType.STRING)
+	 Role role;
+	 
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+
+		return List.of(new SimpleGrantedAuthority(role.name()));
+	}
+	
+	@Override
+	public boolean isAccountNonExpired(){
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked(){
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired(){
+		return true;
+	}
+	@Override
+	public boolean isEnabled(){
+		return true;
+	}
+	
+	
+}
